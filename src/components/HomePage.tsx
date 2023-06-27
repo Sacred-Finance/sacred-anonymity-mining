8b9aee83eb532ff6477a8e39b7e13a0fcbca8460
laughingwhales <worthingtravis@gmail.com>
2023-06-27 12:07:35 -0700

    ## src/components/HomePage.tsx :lipstick: Adjust styling for responsiveness and readability

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -133,12 +133,12 @@ function HomePage({ createCommunity, isAdmin = false }: HomeProps) {
   }
 
   return (
-    <main className="mx-auto w-full max-w-screen-xl space-y-12 p-24  md:px-0 h-full">
-      <div className="flex flex-col items-center py-16 text-purple-500 dark:text-purple-500">
+    <main className="xs:flex xs:flex-col xs:mx-0 xs:p-0 xs:text-center xs:align-center h-full w-full max-w-screen-xl space-y-12 sm:mx-auto  sm:p-24 md:px-0">
+      <div className="flex flex-col items-center px-8 py-16 text-purple-500 dark:text-purple-500">
         <h1 className="mb-8 text-6xl font-bold">{t('pageTitle.communities')}</h1>
         <div className="mb-8 flex w-full flex-col justify-between md:max-w-lg md:flex-row">
-          <div className="relative mb-4 w-full md:mr-2 md:mb-0">
-            <MagnifyingGlassIcon className="absolute top-0 left-0 ml-3 mt-3 h-5 w-5 fill-current text-gray-500 dark:text-gray-400" />
+          <div className="relative mb-4 w-full md:mb-0 md:mr-2">
+            <MagnifyingGlassIcon className="absolute left-0 top-0 ml-3 mt-3 h-5 w-5 fill-current text-gray-500 dark:text-gray-400" />
             <input
               id="search"
               name="search"
@@ -177,7 +177,7 @@ function HomePage({ createCommunity, isAdmin = false }: HomeProps) {
         </div>
       </div>
 
-      <div className="row-gap-8 mb-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="row-gap-8 mb-8 grid grid-cols-1 justify-items-center   gap-4 sm:grid-cols-1 md:grid-cols-2 md:justify-items-center lg:grid-cols-3">
         {localCommunities.map((community, index) => (
           <CommunityCard key={index} community={community} index={index} isAdmin={isAdmin} />
         ))}
@@ -186,16 +186,18 @@ function HomePage({ createCommunity, isAdmin = false }: HomeProps) {
       {localCommunities?.length === 0 && (
         <div className="my-12 flex flex-col items-center justify-center space-y-4">
           <motion.h2
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
             className="text-3xl font-semibold dark:text-gray-200"
           >
             Oops, No Results Found
           </motion.h2>
           <motion.p
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             className="px-4 text-center text-lg dark:text-gray-300"
           >
@@ -211,7 +213,7 @@ function HomePage({ createCommunity, isAdmin = false }: HomeProps) {
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5, delay: 0.4 }}
-            className="rounded bg-purple-500 py-2 px-4 font-bold text-white shadow dark:bg-purple-700 dark:text-gray-200"
+            className="rounded bg-purple-500 px-4 py-2 font-bold text-white shadow dark:bg-purple-700 dark:text-gray-200"
             onClick={() => createCommunity()}
           >
             Create a new community
@@ -223,7 +225,7 @@ function HomePage({ createCommunity, isAdmin = false }: HomeProps) {
         <div className="my-12 flex flex-col items-center justify-center">
           <p className="mb-4 text-xl dark:text-gray-200">0 Communities :(</p>
           <button
-            className="rounded bg-purple-500 py-2 px-4 font-bold text-white shadow dark:bg-purple-700 dark:text-gray-200"
+            className="rounded bg-purple-500 px-4 py-2 font-bold text-white shadow dark:bg-purple-700 dark:text-gray-200"
             onClick={() => createCommunity()}
           >
             Create
