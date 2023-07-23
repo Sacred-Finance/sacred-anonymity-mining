5f7353a4ae110d71e0e8f02e388f39cf06fa99e0
laughingwhales <worthingtravis@gmail.com>
2023-07-22 17:02:44 -0700

    🚀 feat: Implement data fetching and dispatching in WithStandardLayout.tsx
    - Import `useSWR` from "swr"
    - Import `useCommunityContext` from "@/contexts/CommunityProvider"
    - Import `BigNumber` from "ethers"
    - Import `LoadingComponent` from "@components/LoadingComponent"
    
    - Add `useSWR` hook to fetch data from "/api/data"
    - Add `useCommunityContext` hook to access community context dispatch function
    - Convert id back to `BigNumber` in `communitiesData` using `map`
    - Dispatch `SET_COMMUNITIES` action with converted `communitiesData`
    - Dispatch `SET_USERS` action with `users` data
    
    - Handle error case by rendering an error message
    - Handle loading state by rendering `LoadingComponent`
    
    - Pass fetched `data` as additional props to `WrappedComponent`
    
    ## src/components/HOC/WithStandardLayout.tsx

--- src/components/HOC/WithStandardLayout.tsx
+++ src/components/HOC/WithStandardLayout.tsx
@@ -3,6 +3,10 @@ import Header from '@components/Header'
 import { Breadcrumbs } from '@components/Breadcrumbs'
 import Footer from '@components/Footer'
 import { useRouter } from 'next/router'
+import useSWR from "swr";
+import {useCommunityContext} from "@/contexts/CommunityProvider";
+import {BigNumber} from "ethers";
+import LoadingComponent from "@components/LoadingComponent";
 
 export default function WithStandardLayout(WrappedComponent, additionalProps = {}) {
   const WithStandardLayout: (props) => JSX.Element = props => {
@@ -14,12 +18,36 @@ export default function WithStandardLayout(WrappedComponent, additionalProps = {
           console.log('scrolling to top')
       }
     }, [router.pathname])
+
+      const { data, error } = useSWR('/api/data')
+      const { dispatch } = useCommunityContext()
+
+      useEffect(() => {
+          if (!data) return
+          const { communitiesData, users } = data
+          // convert id back to bignumber
+          dispatch({ type: 'SET_COMMUNITIES', payload: communitiesData.map(c => ({ ...c, id: BigNumber.from(c.id) })) })
+          dispatch({
+              type: 'SET_USERS',
+              payload: users,
+          })
+      }, [data])
+
+      if (error) {
+          return <div>Error: {error.message}</div>
+      }
+      if (!data) {
+          return <LoadingComponent/>
+      }
+
+
+
     return (
       <div className={'flex h-screen flex-col'} ref={pageRef}>
         <Header />
         <Breadcrumbs />
         <div>
-          <WrappedComponent {...props} {...additionalProps} />
+          <WrappedComponent {...props} {...additionalProps} {...data}  />
         </div>
         <div className={'flex-1'} />
         <div className={'relative'}>
