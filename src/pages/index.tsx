a0e7243b5bd1385844392c3384924fd8ef10d0d5
Sarkazein <sarkazein7@gmail.com>
2023-10-10 12:13:23 +0530

    fetch list of communities for discourse tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import { BigNumber } from 'ethers'
 import WithStandardLayout from '@components/HOC/WithStandardLayout'
 import HomePage from '../components/HomePage'
+import axios from 'axios'
 
-function Home({ communitiesData, users }) {
+function Home({ communitiesData, users, discourseCommunities }) {
   return (
     <HomePage
       isAdmin={false}
       communities={communitiesData.map(c => ({ ...c, id: BigNumber.from(c.id) }))}
+      discourseCommunities={discourseCommunities}
     />
   )
+}
+
+export const getServerSideProps = async () => {
+  const data = await axios.get(
+    process.env.NEXT_PUBLIC_DISCOURSE_GOOGLE_SHEET_API_URL
+  )
+  console.log(data)
+  return {
+    props: {
+      discourseCommunities: data.data?.communities,
+    },
+  }
 }
 
 export default WithStandardLayout(Home)
