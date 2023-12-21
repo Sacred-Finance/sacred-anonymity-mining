bcaae5e69a1f9b44253db0fa1484a9d1679d5674
laughingwhales <worthingtravis@gmail.com>
2023-12-21 12:43:54 -0800

    hotfix postid

--- src/pages/api/groupWithPostData.ts
+++ src/pages/api/groupWithPostData.ts
@@ -1,16 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next/types'
-import { forumContract, ForumContractAddress } from '@/constant/const'
+import { forumContract } from '@/constant/const'
 import { augmentGroupData, augmentItemData } from '@/utils/communityUtils'
-import type {
-  Group,
-  Item,
-  RawGroupData,
-  RawItemData,
-} from '@/types/contract/ForumInterface'
+import type { Group, Item, RawGroupData, RawItemData } from '@/types/contract/ForumInterface'
 import type { User } from '@/lib/model'
-import { multicall } from '@wagmi/core'
-import { abi } from '@/constant/abi'
-import { polygonMumbai } from 'wagmi/chains'
 import { HashZero } from '@/lib/utils'
 
 export type GroupWithPostDataResponse = {
@@ -22,16 +14,12 @@ export type GroupWithPostDataResponse = {
 // get group details
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<
-    { group: Group; posts: Item[]; users: User[] } | { error: string }
-  >
+  res: NextApiResponse<{ group: Group; posts: Item[]; users: User[] } | { error: string }>
 ) {
   try {
     const { groupId } = req.query
     const bigIntGroupId = BigInt(groupId as string)
-    const rawGroupData = (await forumContract.read.groupAt([
-      bigIntGroupId,
-    ])) as unknown as RawGroupData
+    const rawGroupData = (await forumContract.read.groupAt([bigIntGroupId])) as unknown as RawGroupData
 
     if (rawGroupData.removed) {
       return res.status(404).json({ error: 'Group not found' })
@@ -39,27 +27,13 @@ export default async function handler(
 
     const group = await augmentGroupData(rawGroupData)
 
-    const wagmigotchiContract = {
-      address: ForumContractAddress,
-      abi: abi,
-    } as const
-
-    const postData = rawGroupData.posts.map(postId => ({
-      address: ForumContractAddress as `0x${string}`,
-      functionName: 'itemAt',
-      abi: wagmigotchiContract.abi,
-      args: [postId],
-    }))
-
     const rawPostResults = (await Promise.all(
-      postData.map(async (data, i) => {
+      rawGroupData.posts.map(async (postId, i) => {
         try {
-          return await forumContract.read.itemAt([postIds[i]])
+          return await forumContract.read.itemAt([postId])
         } catch (err) {
           console.error('Error fetching post:', err)
-          return res
-            .status(500)
-            .json({ error: 'An error occurred while fetching post' })
+          return res.status(500).json({ error: 'An error occurred while fetching post' })
         }
       })
     )) as unknown as RawItemData[]
@@ -74,17 +48,13 @@ export default async function handler(
           return await augmentItemData(post)
         } catch (err) {
           console.error('Error augmenting post:', err)
-          return res
-            .status(500)
-            .json({ error: 'An error occurred while augmenting post' })
+          return res.status(500).json({ error: 'An error occurred while augmenting post' })
         }
       })
       // @ts-expect-error: filter removes nulls, needs to be fixed
     )?.then(posts => posts.filter((p: Item) => !p.removed))) as Item[]
 
-    const usersWithCommitment = await forumContract.read.groupUsers([
-      bigIntGroupId,
-    ])
+    const usersWithCommitment = await forumContract.read.groupUsers([bigIntGroupId])
 
     return res.status(200).json(<GroupWithPostDataResponse>{
       group,
