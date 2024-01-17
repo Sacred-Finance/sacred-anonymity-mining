d8e22670e7fdc868cc94fce8b30eecd141a7c1bf
Travis <WorthingTravis@gmail.com>
2024-01-17 07:28:37 -0800

    fix-admin-functions

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,34 +1,24 @@
-import React, {useEffect, useState} from 'react'
-import {useAccount, useContractWrite} from 'wagmi'
+import React, { useEffect, useState } from 'react'
+import { useAccount, useContractWrite } from 'wagmi'
 import ForumABI from '../../constant/abi/Forum.json'
-import {useRouter} from 'next/router'
-import {useTranslation} from 'react-i18next'
-import {ForumContractAddress} from '@/constant/const'
-import {useCheckIfUserIsAdminOrModerator} from '@/hooks/useCheckIfUserIsAdminOrModerator'
-import {useFetchAdminsAndModerators} from '@/hooks/useFetchAdminsAndModerators'
-import type {Address} from '@/types/common'
-import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow,} from '@/shad/ui/table'
-import {PlusCircleIcon, TrashIcon} from '@heroicons/react/20/solid'
-import {PrimaryButton} from '@/components/buttons'
-import {toast} from 'react-toastify'
+import { useTranslation } from 'react-i18next'
+import { ForumContractAddress } from '@/constant/const'
+import { useCheckIfUserIsAdminOrModerator } from '@/hooks/useCheckIfUserIsAdminOrModerator'
+import { useFetchAdminsAndModerators } from '@/hooks/useFetchAdminsAndModerators'
+import type { Address } from '@/types/common'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/shad/ui/table'
+import { PlusCircleIcon, TrashIcon } from '@heroicons/react/20/solid'
+import { PrimaryButton } from '@/components/buttons'
+import { toast } from 'react-toastify'
 import LoadingComponent from '@/components/LoadingComponent'
 import clsx from 'clsx'
 
 const Access: React.FC = () => {
   const { address } = useAccount()
-  const navigate = useRouter()
   const { t } = useTranslation()
 
-  const { isAdmin, isModerator, fetchIsAdmin } =
-    useCheckIfUserIsAdminOrModerator(true)
-  const {
-    isFetching,
-    isLoading,
-    admins,
-    moderators,
-    fetchAdmins,
-    fetchModerators,
-  } = useFetchAdminsAndModerators()
+  const { isAdmin, isModerator, fetchIsAdmin } = useCheckIfUserIsAdminOrModerator(true)
+  const { isFetching, isLoading, admins, moderators, fetchAdmins, fetchModerators } = useFetchAdminsAndModerators()
 
   useEffect(() => {
     if (address) {
@@ -39,129 +29,68 @@ const Access: React.FC = () => {
   }, [address])
 
   /** remove admin */
-  const { write: writeRemoveAdmin, isLoading: isRemovingAdmin } =
-    useContractWrite({
+
+  const useForumContractWrite = (
+    functionName: string,
+    onSuccessCallback: (data: any, variables?: any) => Promise<void>
+  ) => {
+    return useContractWrite({
       address: ForumContractAddress as Address,
       abi: ForumABI.abi,
-      functionName: 'removeAdmin',
-      mode: 'recklesslyUnprepared',
-      onSettled: (data, error) => {
-        // setIsLoading(false)
-      },
-      onSuccess: async (data, variables) => {
-        try {
-          console.log('data', data)
-          console.log('variables', variables)
-          await data.wait()
-          fetchAdmins()
-          fetchIsAdmin()
-
-        } catch (error) {}
-      },
-      onError: (error, variables) => {
-        toast.error(error.message, { autoClose: 5000 })
+      functionName,
+      onSuccess: onSuccessCallback,
+      onError: error => {
+        toast.error(error.stack, { autoClose: 5000 })
       },
     })
+  }
 
-  /** remove moderator */
-  const { write: writeRemoveModerator, isLoading: isRemovingMod } =
-    useContractWrite({
-      address: ForumContractAddress as Address,
-      abi: ForumABI.abi,
-      functionName: 'removeModerators',
-      mode: 'recklesslyUnprepared',
-      onSettled: (data, error) => {
-        // setIsLoading(false)
-      },
-      onSuccess: async (data, variables) => {
-        try {
-          await data.wait()
-          fetchModerators()
+  const onAddOrRemove = (writeFunction: any, value: string) => {
+    if (writeFunction) {
+      writeFunction({
+        args: [[value]],
+      })
+    }
+  }
 
-        } catch (error) {}
-      },
-      onError: (error, variables) => {
-        toast.error(error.message, { autoClose: 5000 })
-      },
-    })
-
-  /** add admin */
-  const { write: writeAddAdmin, isLoading: isAddingAdmin } = useContractWrite({
-    address: ForumContractAddress as Address,
-    abi: ForumABI.abi,
-    functionName: 'addAdmins',
-    mode: 'recklesslyUnprepared',
-    onSettled: (data, error) => {
-      // setIsLoading(false)
-    },
-    onSuccess: async (data, variables) => {
-      try {
-        console.log('data', data)
-        console.log('variables', variables)
-        await data.wait()
-        fetchAdmins()
-
-      } catch (error) {}
-    },
-    onError: (error, variables) => {
-      toast.error(error.message, { autoClose: 5000 })
-    },
-  })
-
-  /** add moderator */
-  const { write: writeAddModerator, isLoading: isAddingMod } = useContractWrite(
-    {
-      address: ForumContractAddress as Address,
-      abi: ForumABI.abi,
-      functionName: 'addModerators',
-      mode: 'recklesslyUnprepared',
-      onSettled: (data, error) => {
-        // setIsLoading(false)
-      },
-      onSuccess: async (data, variables) => {
-        try {
-          await data.wait()
-          fetchModerators()
-
-        } catch (error) {}
-      },
-      onError: (error, variables) => {
-        toast.error(error.message, { autoClose: 5000 })
-      },
+  // Use case for adding/removing admins and moderators
+  const { write: writeAddAdmin, isLoading: isAddingAdmin } = useForumContractWrite(
+    'addAdmins',
+    async (data, variables) => {
+      console.log('data', data)
+      console.log('variables', variables)
+      await data.wait()
+      await fetchAdmins()
     }
   )
 
-  const onAddAdmin = value => {
-    if (writeAddAdmin) {
-      writeAddAdmin({
-        recklesslySetUnpreparedArgs: [[value]],
-      })
-    }
-  }
+  const { write: writeRemoveAdmin, isLoading: isRemovingAdmin } = useForumContractWrite('removeAdmin', async data => {
+    await data.wait()
+    fetchAdmins()
+    fetchIsAdmin()
+  })
 
-  const onAddModerator = value => {
-    if (writeAddModerator) {
-      writeAddModerator({
-        recklesslySetUnpreparedArgs: [[value]],
-      })
+  const { write: writeAddModerator, isLoading: isAddingModerator } = useForumContractWrite(
+    'addModerators',
+    async data => {
+      await data.wait()
+      fetchModerators()
     }
-  }
+  )
 
-  const onRemoveAdmin = value => {
-    if (writeRemoveAdmin) {
-      writeRemoveAdmin({
-        recklesslySetUnpreparedArgs: [value],
-      })
+  const { write: writeRemoveModerator, isLoading: isRemovingModerator } = useForumContractWrite(
+    'removeModerators',
+    async data => {
+      await data.wait()
+      fetchModerators()
     }
-  }
+  )
 
-  const onRemoveModerator = value => {
-    if (writeRemoveModerator) {
-      writeRemoveModerator({
-        recklesslySetUnpreparedArgs: [[value]],
-      })
-    }
-  }
+  // Handlers
+  const onAddAdmin = (value: string) => onAddOrRemove(writeAddAdmin, value)
+  const onRemoveAdmin = (value: string) => onAddOrRemove(writeRemoveAdmin, value)
+  const onAddModerator = (value: string) => onAddOrRemove(writeAddModerator, value)
+  const onRemoveModerator = (value: string) => onAddOrRemove(writeRemoveModerator, value)
 
   const AddressInput = ({ placeholder, onSubmit }) => {
     const [value, setValue] = useState('')
@@ -170,10 +99,7 @@ const Access: React.FC = () => {
       <>
         <div className="relative">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <PlusCircleIcon
-              className="h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
+            <PlusCircleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
           </div>
           <input
             type="input"
@@ -189,13 +115,16 @@ const Access: React.FC = () => {
             className="hover:bg-primary-800 dark:hover:bg-primary-700 dark:focus:ring-primary-800 absolute bottom-2.5 right-2.5 top-[7px] rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-primary"
             onClick={() => onSubmit(value)}
             disabled={!value || Boolean(patternError)}
-            isLoading={isAddingAdmin || isAddingMod}
+            isLoading={
+              isAddingAdmin || isRemovingAdmin || isAddingModerator || isRemovingModerator || isFetching || isLoading
+            }
           >
             Add
           </PrimaryButton>
         </div>
-        <div className={clsx([patternError ? 'visible text-red-800' : 'invisible', 'mt-0'])}>{t('formErrors.enterValidAddress')}</div>
-        }
+        <div className={clsx([patternError ? 'visible text-red-800' : 'invisible', 'mt-0'])}>
+          {t('formErrors.enterValidAddress')}
+        </div>
       </>
     )
   }
@@ -219,10 +148,7 @@ const Access: React.FC = () => {
                 <TableCell>{a}</TableCell>
                 <TableCell>
                   {canRemove && (
-                    <button
-                      onClick={() => onRemove(a)}
-                      aria-label="remove admin"
-                    >
+                    <button onClick={() => onRemove(a)} aria-label="remove admin">
                       <TrashIcon className="h-5 w-5" />
                     </button>
                   )}
@@ -235,8 +161,7 @@ const Access: React.FC = () => {
     )
   }
 
-  if (!isAdmin && !isModerator && (isFetching || isLoading))
-    return <LoadingComponent />
+  if (!isAdmin && !isModerator && (isFetching || isLoading)) return <LoadingComponent />
 
   if (!isAdmin && !isModerator) {
     return <div>Not allowed to access </div>
@@ -247,30 +172,19 @@ const Access: React.FC = () => {
         <h1 className="text-left text-[22px] font-bold" color="primary.500">
           Admins
         </h1>
-        <AddressInput
-          placeholder={'Admin Address'}
-          onSubmit={v => onAddAdmin(v)}
-        ></AddressInput>
-        <TableWrapper
-          caption="Admins"
-          items={admins}
-          canRemove={isAdmin}
-          onRemove={v => onRemoveAdmin(v)}
-        ></TableWrapper>
+        <AddressInput placeholder="Admin Address" onSubmit={v => onAddAdmin(v)}></AddressInput>
+        <TableWrapper caption="Admins" items={admins} canRemove={isAdmin} onRemove={v => onRemoveAdmin(v)} />
 
         <h1 className="text-left text-[22px] font-bold" color="primary.500">
           Moderators
         </h1>
-        <AddressInput
-          placeholder={'Moderator Address'}
-          onSubmit={v => onAddModerator(v)}
-        ></AddressInput>
+        <AddressInput placeholder="Moderator Address" onSubmit={v => onAddModerator(v)}></AddressInput>
         <TableWrapper
           caption="Moderators"
           items={moderators}
           canRemove={isAdmin || isModerator}
           onRemove={v => onRemoveModerator(v)}
-        ></TableWrapper>
+        />
       </div>
     </div>
   )
